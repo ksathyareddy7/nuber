db1dbd632dfae471046867ce8a07b40dfbb40406
sathya reddy <ksathyareddy7@gmail.com>
2021-04-08 17:34:03 +0530

    added validator package

---
+++ backend/src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,16 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { Restaurant } from './entities/restaurant.entity';
+
+// return function of type in resolver is optional
+@Resolver((of) => Restaurant)
+export class RestaurantsResolver {
+  @Query((returns) => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    return true;
+  }
+}
