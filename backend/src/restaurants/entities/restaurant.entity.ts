1c59878991b911994d84a2be0bbd65296b87b3e1
sathya reddy <ksathyareddy7@gmail.com>
2021-04-21 22:31:46 +0530

    added database connection

--- backend/src/restaurants/entities/restaurant.entity.ts
+++ backend/src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,28 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@InputType({ isAbstract: true })
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @Field((type) => Number)
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @Field((type) => String)
+  @Column()
+  @IsString()
+  @Length(5)
   name: string;
 
   @Field((type) => Boolean)
+  @Column()
+  @IsBoolean()
   isVegan?: boolean;
 
   @Field((type) => String)
+  @Column()
+  @IsString()
   address: string;
-
-  @Field((type) => String)
-  ownerName: string;
 }
--- backend/src/restaurants/restaurants.resolver.ts
+++ backend/src/restaurants/restaurants.resolver.ts
@@ -1,16 +1,25 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurants.service';
 
 // return function of type in resolver is optional
 @Resolver((of) => Restaurant)
 export class RestaurantsResolver {
+  constructor(private readonly restaurantService: RestaurantService) {}
   @Query((returns) => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
   @Mutation((returns) => Boolean)
-  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
-    return true;
+  async createRestaurant(
+    @Args('input') createRestaurantDto: CreateRestaurantDto,
+  ): Promise<boolean> {
+    try {
+      await this.restaurantService.createRestaurant(createRestaurantDto);
+      return true;
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
